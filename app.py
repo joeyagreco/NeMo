15b20feeeb845634621051d352c035cc08657b3d
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-13 15:48:29 -0600

    refactoring folder structures

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+import os
+
 from flask import Flask
+from server.util.EnvironmentReader import EnvironmentReader
 
 from server.Pinger import Pinger
-from util.EnvironmentReader import EnvironmentReader
 
-app = Flask(__name__)
+# set directories
+templateDir = os.path.join(os.path.dirname(__file__), "client//templates")
+staticDir = os.path.join(os.path.dirname(__file__), "client//static")
+app = Flask(__name__, template_folder=templateDir, static_folder=staticDir)
 
 if __name__ == "__main__":
-    from controllers.adminController import *
-    from controllers.deviceController import *
 
     test = Pinger.ping("8.8.8.8")
     print(test)
