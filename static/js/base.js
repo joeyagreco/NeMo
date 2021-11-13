0237923f15b230e430dc7b44f810cecfdfb9b59a
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-12 19:54:00 -0600

    added logic for validating data in device form

--- static/js/base.js
+++ static/js/base.js
@@ -17,10 +17,23 @@ function onOpenDeviceForm() {
 function onCloseDeviceForm() {
     document.getElementById("deviceForm").style.display = "none";
     document.getElementById("formBackground").style.display = "none";
-//    document.getElementById("delete-button").style.display = "none";
-//    document.getElementById("submit-button").classList.add("disabled");
+    document.getElementById("delete-button").style.display = "none";
+    document.getElementById("submit-button").classList.add("disabled");
 //    document.getElementById("submit-button").innerHTML = "Submit";
 //    setTransactionPopupToDefaultValues();
+}
+
+function activateSubmitButtonIfValidInput() {
+    const deviceName = document.getElementById("deviceNameInput").value;
+    const ipAddress = document.getElementById("ipAddressInput").value;
+    console.log(ipAddress.split(".").length);
+    if(deviceName.length > 0 && ipAddress.length >= 7 && ipAddress.split(".").length == 4) {
+        document.getElementById("submitButton").classList.remove("disabled");
+        document.getElementById("submitButton").disabled = false;
+    } else {
+        document.getElementById("submitButton").classList.add("disabled");
+        document.getElementById("submitButton").disabled = true;
+    }
 }
 
 document.onreadystatechange = function() {
@@ -28,5 +41,6 @@ document.onreadystatechange = function() {
         startLoading();
     } else {
         stopLoading();
+        onOpenDeviceForm();
     }
 }
