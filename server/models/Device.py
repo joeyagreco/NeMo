a8058904806254b46633b614ff67a9d4b8a250e7
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-15 19:56:11 -0600

    cleanup

--- server/models/Device.py
+++ server/models/Device.py
@@ -3,10 +3,10 @@ from server.enums.DeviceRank import DeviceRank
 
 class Device:
     def __init__(self, name: str, rank: DeviceRank, ipAddress: str, **params):
-        self.id = params.pop("id", None)
-        self.rank = rank
         self.name = name
+        self.rank = rank
         self.ipAddress = ipAddress
+        self.id = params.pop("id", None)
         self.lastAliveTimestamp = params.pop("lastAliveTimestamp",
                                              None)  # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Timestamp.html
 
