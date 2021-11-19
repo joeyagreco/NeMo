ae97fea5b83051e70b6bf0fa49df0206e521f25f
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-18 18:45:23 -0600

    added comments

--- server/models/DeviceFE.py
+++ server/models/DeviceFE.py
@@ -10,7 +10,7 @@ class DeviceFE(Device):
     def __init__(self, name: str, rank: DeviceRank, ipAddress: str, **params):
         super().__init__(name, rank, ipAddress, **params)
         self.lastAliveTimestamp = params.pop("lastAliveTimestamp",
-                                             None)  # Timestamp # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Timestamp.html
+                                             None)  # Timestamp https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Timestamp.html
         self.status = params.pop("status", None)  # Status Enum
 
     def __str__(self):
