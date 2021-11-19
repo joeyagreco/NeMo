1fb69878a273bc6a8373a02e2c738850a060763e
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-18 18:53:13 -0600

    added dummy flow to device service

--- server/controllers/adminController.py
+++ server/controllers/adminController.py
@@ -1,10 +1,8 @@
-import pandas as pd
 from flask import redirect, url_for, render_template
 
 from app import app
 from server.enums.DeviceRank import DeviceRank
-from server.enums.Status import Status
-from server.models.DeviceFE import DeviceFE
+from server.services.DeviceService import DeviceService
 
 
 @app.route('/favicon.ico')
@@ -17,11 +15,6 @@ def favicon():
 
 @app.route("/")
 def index():
-    d1 = DeviceFE("Router", DeviceRank.CRITICAL, "192.168.1.1", id="1",
-                  lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.ONLINE)
-    d2 = DeviceFE("Switch", DeviceRank.CRITICAL, "192.168.1.5", id="2",
-                  lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.SHAKY)
-    d3 = DeviceFE("JAccessPoint", DeviceRank.CRITICAL, "192.168.1.10", id="3",
-                  lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.OFFLINE)
-    devices = [d1, d2, d3]
-    return render_template("homePage/homePage.html", devices=devices, device_rank_class=DeviceRank)
+    deviceService = DeviceService()
+    criticalDevices = deviceService.getDevicesByDeviceRank(DeviceRank.CRITICAL)
+    return render_template("homePage/homePage.html", critical_devices=criticalDevices, device_rank_class=DeviceRank)
---
+++ server/services/DeviceService.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import pandas as pd
+
+from server.enums.DeviceRank import DeviceRank
+from server.enums.Status import Status
+from server.models.Device import Device
+from server.models.DeviceFE import DeviceFE
+
+
+class DeviceService:
+
+    def __init__(self):
+        return
+
+    def getDevicesByDeviceRank(self, deviceRank: DeviceRank) -> List[Device]:
+        d1 = DeviceFE("Router", DeviceRank.CRITICAL, "192.168.1.1", id="1",
+                      lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.ONLINE)
+        d2 = DeviceFE("Switch", DeviceRank.CRITICAL, "192.168.1.5", id="2",
+                      lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.SHAKY)
+        d3 = DeviceFE("JAccessPoint", DeviceRank.CRITICAL, "192.168.1.10", id="3",
+                      lastAliveTimestamp=pd.Timestamp('2017-01-01T12'), status=Status.OFFLINE)
+        devices = [d1, d2, d3]
+        return devices
