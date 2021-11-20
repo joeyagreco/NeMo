178cc875f4f25e63c737830eced5dc742bda20e5
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-20 11:09:09 -0600

    added basic validation for settings page

--- client/static/js/settingsPage.js
+++ client/static/js/settingsPage.js
@@ -20,3 +20,21 @@ function putSettings() {
         window.location.href = response.url;
     });
 }
+
+function activateUpdateButtonIfValidInput() {
+    const pingsToSave = document.getElementById("pingsToSaveInput").value;
+    const pingOnlineThreshold = document.getElementById("pingOnlineThresholdInput").value;
+    const pageRefreshFrequency = document.getElementById("pageRefreshFrequencyInput").value;
+    const pingCriticalRefreshFrequency = document.getElementById("pingCriticalRefreshFrequencyInput").value;
+    const pingKnownRefreshFrequency = document.getElementById("pingKnownRefreshFrequencyInput").value;
+    const pingScanFrequency = document.getElementById("pingScanFrequencyInput").value;
+
+    if (pingsToSave > 0 && pingOnlineThreshold <= 100 && pingOnlineThreshold > 0 && pageRefreshFrequency > 0 && pingCriticalRefreshFrequency > 0 && pingKnownRefreshFrequency > 0 && pingScanFrequency > 0) {
+        document.getElementById("updateSettingsButton").classList.remove("disabled");
+        document.getElementById("updateSettingsButton").disabled = false;
+    } else {
+        document.getElementById("updateSettingsButton").classList.add("disabled");
+        document.getElementById("updateSettingsButton").disabled = true;
+    }
+
+}
