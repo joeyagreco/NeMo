00a8fcbc95b42d449bbcd08444b825ca62b8302f
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-20 11:09:24 -0600

    cleanup

--- client/static/js/settingsPage.js
+++ client/static/js/settingsPage.js
@@ -29,7 +29,8 @@ function activateUpdateButtonIfValidInput() {
     const pingKnownRefreshFrequency = document.getElementById("pingKnownRefreshFrequencyInput").value;
     const pingScanFrequency = document.getElementById("pingScanFrequencyInput").value;
 
-    if (pingsToSave > 0 && pingOnlineThreshold <= 100 && pingOnlineThreshold > 0 && pageRefreshFrequency > 0 && pingCriticalRefreshFrequency > 0 && pingKnownRefreshFrequency > 0 && pingScanFrequency > 0) {
+    if (pingsToSave > 0 && pingOnlineThreshold <= 100 && pingOnlineThreshold > 0 && pageRefreshFrequency > 0
+        && pingCriticalRefreshFrequency > 0 && pingKnownRefreshFrequency > 0 && pingScanFrequency > 0) {
         document.getElementById("updateSettingsButton").classList.remove("disabled");
         document.getElementById("updateSettingsButton").disabled = false;
     } else {
