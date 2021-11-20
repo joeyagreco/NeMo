5a9adea9fc6ebcec8e11a5fa0621608b7bec8a6f
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-20 11:50:50 -0600

    established database connection

--- server/repositories/SettingsRepository.py
+++ server/repositories/SettingsRepository.py
@@ -1,4 +1,20 @@
+import psycopg2
+
+from server.util.EnvironmentReader import EnvironmentReader
+
+
 class SettingsRepository:
 
     def __init__(self):
-        return
+        self.conn = psycopg2.connect(
+            host=EnvironmentReader.get("DB_HOST"),
+            database=EnvironmentReader.get("DB_DATABASE"),
+            user=EnvironmentReader.get("DB_USER"),
+            password=EnvironmentReader.get("DB_PASSWORD"))
+
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("select * from nemo.setting")
+        result = self.cursor.fetchall()
+        print(result)
+
+        self.conn.close()
