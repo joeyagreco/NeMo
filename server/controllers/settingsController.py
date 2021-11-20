f6ec6b305561c6f2c5b8e422c0214b411b9bfb4d
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-20 10:39:48 -0600

    added model and controller for settings

---
+++ server/controllers/settingsController.py
@@ -0,0 +1,16 @@
+from flask import redirect, url_for, request
+
+from app import app
+from server.models.Setting import Setting
+from server.util.ControllerHelper import ControllerHelper
+
+
+@app.route("/settings", methods=["PUT"])
+def updateSettings():
+    # convert the PUT request headers into a python dictionary
+    dataDict = ControllerHelper.getDictFromRequestObj(request)
+    setting = Setting(dataDict["pingsToSave"], dataDict["pingOnlineThreshold"], dataDict["pageRefreshFrequency"],
+                      dataDict["pingCriticalRefreshFrequency"], dataDict["pingKnownRefreshFrequency"],
+                      dataDict["pingScanFrequency"])
+    print(setting)
+    return redirect(url_for("settingsPage"))
---
+++ server/models/Setting.py
@@ -0,0 +1,15 @@
+class Setting:
+    def __init__(self, pingsToSave: int, pingOnlineThreshold: int, pageRefreshFrequency: int,
+                 pingCriticalRefreshFrequency: int, pingKnownRefreshFrequency: int, pingScanFrequency: int):
+        self.pingsToSave = pingsToSave
+        self.pingOnlineThreshold = pingOnlineThreshold
+        self.pageRefreshFrequency = pageRefreshFrequency
+        self.pingCriticalRefreshFrequency = pingCriticalRefreshFrequency
+        self.pingKnownRefreshFrequency = pingKnownRefreshFrequency
+        self.pingScanFrequency = pingScanFrequency
+
+    def __str__(self):
+        return f"pingsToSave: {self.pingsToSave}\npingOnlineThreshold: {self.pingOnlineThreshold}\npageRefreshFrequency: {self.pageRefreshFrequency}\npingCriticalRefreshFrequency: {self.pingCriticalRefreshFrequency}\npingKnownRefreshFrequency: {self.pingKnownRefreshFrequency}\npingScanFrequency: {self.pingScanFrequency}"
+
+    def __repr__(self):
+        return f"pingsToSave: {self.pingsToSave}\npingOnlineThreshold: {self.pingOnlineThreshold}\npageRefreshFrequency: {self.pageRefreshFrequency}\npingCriticalRefreshFrequency: {self.pingCriticalRefreshFrequency}\npingKnownRefreshFrequency: {self.pingKnownRefreshFrequency}\npingScanFrequency: {self.pingScanFrequency}"
