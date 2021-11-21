4ddd2f49a7df03c0d62ad38d595088c58d20a27f
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-20 23:56:45 -0600

    got basic test working with mock

---
+++ server/test/test_services/Test_DeviceService.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import patch
+
+from server.repositories.DeviceRepository import DeviceRepository
+from server.services.DeviceService import DeviceService
+
+
+class Test_DeviceService(unittest.TestCase):
+
+    def setUp(self):
+        self.deviceService = DeviceService()
+
+    @patch.object(DeviceRepository, "getAllDevices", return_value=[])
+    def test_test(self, mocks):
+        print(self.deviceService.getAllDevicesFE())
+        self.assertTrue(True)
