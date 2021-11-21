09856c2b9e41cb8e5485c2e5c4eb73a2e8b767fb
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-21 00:50:46 -0600

    test writing

--- server/test/test_services/Test_DeviceService.py
+++ server/test/test_services/Test_DeviceService.py
@@ -20,10 +20,11 @@ class Test_DeviceService(unittest.TestCase):
     @patch("server.services.DeviceService.SettingsService.getSettings")
     def test_getAllDevicesFE_happyPath(self, mockGetSettings, mockGetPingsByDeviceId, mockGetAllDevices):
         dummyDeviceBE = DeviceBE("n", DeviceRank.CRITICAL, "1.1.1.1", id=1)
-        dummyPing = Ping(True, datetime.datetime(2020, 1, 2, 10, 11, 12, 13))
+        dummyPing1 = Ping(True, datetime.datetime(2020, 1, 2, 10, 11, 12, 13))
+        dummyPing2 = Ping(True, datetime.datetime(2020, 1, 2, 10, 11, 13, 13))
         dummySettings = Settings(None, 50, None, None, None, None)
         mockGetAllDevices.return_value = [dummyDeviceBE]
-        mockGetPingsByDeviceId.return_value = [dummyPing]
+        mockGetPingsByDeviceId.return_value = [dummyPing1, dummyPing2]
         mockGetSettings.return_value = dummySettings
         response = self.deviceService.getAllDevicesFE()
         self.assertEqual(1, len(response))
@@ -31,5 +32,5 @@ class Test_DeviceService(unittest.TestCase):
         self.assertEqual(DeviceRank.CRITICAL, response[0].rank)
         self.assertEqual("1.1.1.1", response[0].ipAddress)
         self.assertEqual(1, response[0].id)
-        self.assertTrue(datetime.datetime(2020, 1, 2, 10, 11, 12) == response[0].lastAliveTimestamp)
+        self.assertTrue(datetime.datetime(2020, 1, 2, 10, 11, 13) == response[0].lastAliveTimestamp)
         self.assertEqual(Status.ONLINE, response[0].status)
