6ad878f19b0ea831d5e3d3b96f1b57b0f4a27398
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-21 00:48:01 -0600

    test writing

--- server/test/test_services/Test_DeviceService.py
+++ server/test/test_services/Test_DeviceService.py
@@ -1,8 +1,12 @@
+import datetime
 import unittest
 from unittest.mock import patch
 
-from server.repositories.DeviceRepository import DeviceRepository
-from server.repositories.PingRepository import PingRepository
+from server.enums.DeviceRank import DeviceRank
+from server.enums.Status import Status
+from server.models.DeviceBE import DeviceBE
+from server.models.Ping import Ping
+from server.models.Settings import Settings
 from server.services.DeviceService import DeviceService
 
 
@@ -11,8 +15,21 @@ class Test_DeviceService(unittest.TestCase):
     def setUp(self):
         self.deviceService = DeviceService()
 
-    @patch.object(DeviceRepository, "getAllDevices", return_value=[])
-    @patch.object(PingRepository, "getPingsByDeviceId", return_value=[])
-    def test_test(self, deviceRepositoryMock, pingRepositoryMock):
-        print(self.deviceService.getAllDevicesFE())
-        self.assertTrue(True)
+    @patch("server.services.DeviceService.DeviceRepository.getAllDevices")
+    @patch("server.services.DeviceService.PingRepository.getPingsByDeviceId")
+    @patch("server.services.DeviceService.SettingsService.getSettings")
+    def test_getAllDevicesFE_happyPath(self, mockGetSettings, mockGetPingsByDeviceId, mockGetAllDevices):
+        dummyDeviceBE = DeviceBE("n", DeviceRank.CRITICAL, "1.1.1.1", id=1)
+        dummyPing = Ping(True, datetime.datetime(2020, 1, 2, 10, 11, 12, 13))
+        dummySettings = Settings(None, 50, None, None, None, None)
+        mockGetAllDevices.return_value = [dummyDeviceBE]
+        mockGetPingsByDeviceId.return_value = [dummyPing]
+        mockGetSettings.return_value = dummySettings
+        response = self.deviceService.getAllDevicesFE()
+        self.assertEqual(1, len(response))
+        self.assertEqual("n", response[0].name)
+        self.assertEqual(DeviceRank.CRITICAL, response[0].rank)
+        self.assertEqual("1.1.1.1", response[0].ipAddress)
+        self.assertEqual(1, response[0].id)
+        self.assertTrue(datetime.datetime(2020, 1, 2, 10, 11, 12) == response[0].lastAliveTimestamp)
+        self.assertEqual(Status.ONLINE, response[0].status)
