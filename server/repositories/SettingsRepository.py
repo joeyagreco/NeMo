c9b1ac4e32220d233f0581c556b3352efffee427
jg <57115019+joeyagreco@users.noreply.github.com>
2021-11-22 01:46:15 -0600

    clean up

--- server/repositories/SettingsRepository.py
+++ server/repositories/SettingsRepository.py
@@ -7,10 +7,14 @@ from server.util.EnvironmentReader import EnvironmentReader
 class SettingsRepository:
 
     def __init__(self):
-        self.__conn = None
+        self.__conn = psycopg2.connect(
+            host=EnvironmentReader.get("DB_HOST"),
+            database=EnvironmentReader.get("DB_DATABASE"),
+            user=EnvironmentReader.get("DB_USER"),
+            password=EnvironmentReader.get("DB_PASSWORD"))
         # since we will only ever have 1 row in this table, we know the id will always be 1
-        self.__settingsRowId = 1
-        self.__settingsSchemaAndTableName = "nemo.setting"
+        self.__SETTINGS_ROW_ID = 1
+        self.__SETTINGS_SCHEMA_AND_TABLE_NAME = "nemo.setting"
         self.__getAllSettingsQuery = f"""
                                         select pings_to_save,
                                               ping_online_threshold_percentage,
@@ -18,7 +22,7 @@ class SettingsRepository:
                                               ping_critical_refresh_frequency_seconds,
                                               ping_known_refresh_frequency_seconds,
                                               ping_scan_frequency_seconds
-                                        from {self.__settingsSchemaAndTableName} where id = {self.__settingsRowId}
+                                        from {self.__SETTINGS_SCHEMA_AND_TABLE_NAME} where id = {self.__SETTINGS_ROW_ID}
                                       """
         self.__updateSettingsQuery = """
                                         update {settingsSchemaAndTableName} set
@@ -31,19 +35,10 @@ class SettingsRepository:
                                         where id = {settingsRowId}
                                      """
 
-    def __connect(self):
-        self.__conn = psycopg2.connect(
-            host=EnvironmentReader.get("DB_HOST"),
-            database=EnvironmentReader.get("DB_DATABASE"),
-            user=EnvironmentReader.get("DB_USER"),
-            password=EnvironmentReader.get("DB_PASSWORD"))
-
-    def __close(self):
+    def __del__(self):
         self.__conn.close()
-        self.__conn = None
 
     def getSettings(self) -> Settings:
-        self.__connect()
         with self.__conn.cursor() as cursor:
             cursor.execute(self.__getAllSettingsQuery)
             result = cursor.fetchone()
@@ -53,20 +48,17 @@ class SettingsRepository:
                                                             result[3],
                                                             result[4],
                                                             result[5])
-        self.__close()
         return settings
 
     def updateSettings(self, settings: Settings) -> None:
-        self.__connect()
         with self.__conn.cursor() as cursor:
             cursor.execute(self.__updateSettingsQuery.format(
-                settingsSchemaAndTableName=self.__settingsSchemaAndTableName,
+                settingsSchemaAndTableName=self.__SETTINGS_SCHEMA_AND_TABLE_NAME,
                 pingsToSave=settings.pingsToSave,
                 pingOnlineThresholdPercentage=settings.pingOnlineThresholdPercentage,
                 pageRefreshFrequencySeconds=settings.pageRefreshFrequencySeconds,
                 pingCriticalRefreshFrequencySeconds=settings.pingCriticalRefreshFrequencySeconds,
                 pingKnownRefreshFrequencySeconds=settings.pingKnownRefreshFrequencySeconds,
                 pingScanFrequencySeconds=settings.pingScanFrequencySeconds,
-                settingsRowId=self.__settingsRowId))
+                settingsRowId=self.__SETTINGS_ROW_ID))
             self.__conn.commit()
-        self.__close()
